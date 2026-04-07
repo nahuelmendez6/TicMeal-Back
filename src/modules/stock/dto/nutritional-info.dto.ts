2b318368019e0370aef7afd260ba959e8e2e46b5
nahuelmendez6 <mendezmaturano6@gmail.com>
2026-04-07 19:50:49 +0200

    fix(dto): fixed nutritional-info.dto to allow the creation of a menu-item with its related nutritional info

--- src/modules/stock/dto/nutritional-info.dto.ts
+++ src/modules/stock/dto/nutritional-info.dto.ts
@@ -1,10 +1,31 @@
+import { IsNumber, IsOptional, IsPositive, Min } from 'class-validator';
+
 // src/modules/stock/dto/nutritional-info.dto.ts
 export class NutritionalInfo {
+  @IsNumber()
+  @IsPositive()
   calories: number; // por 100g o por unidad base
+
+  @IsNumber()
+  @Min(0)
   protein: number; // gramos
+
+  @IsNumber()
+  @Min(0)
   carbohydrates: number; // gramos
+
+  @IsNumber()
+  @Min(0)
   fat: number; // gramos
+
+  @IsOptional()
+  @IsNumber()
+  @Min(0)
   sugar?: number; // gramos
+
+  @IsOptional()
+  @IsNumber()
+  @Min(0)
   sodium?: number; // miligramos
   // ... otros nutrientes que necesites
 }
