9197a51ce30fb17d285b5025403dad9af815b1a6
nahuelmendez6 <mendezmaturano6@gmail.com>
2026-04-16 11:03:21 +0200

    feat: register reservations.controller and reservactions.service on reservations.module

--- src/modules/reservations/reservations.module.ts
+++ src/modules/reservations/reservations.module.ts
@@ -3,16 +3,26 @@ import { TypeOrmModule } from '@nestjs/typeorm';
 import { Reservation } from './entities/reservation.entity';
 import { Timeslot } from './entities/timeslot.entity';
 import { WaitingListEntry } from './entities/waiting-list-entry.entity';
+import { User } from 'src/modules/users/entities/user.entity';
+import { MenuOption } from 'src/modules/menus/entities/menu-option.entity';
 
 import { TimeslotsController } from './controllers/timeslots.controller';
 import { TimeslotsService } from './services/timeslots.service';
+import { ReservationsController } from './controllers/reservations.controller';
+import { ReservationsService } from './services/reservations.service';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Reservation, Timeslot, WaitingListEntry]),
+    TypeOrmModule.forFeature([
+      Reservation,
+      Timeslot,
+      WaitingListEntry,
+      User,
+      MenuOption,
+    ]),
   ],
-  controllers: [TimeslotsController],
-  providers: [TimeslotsService],
-  exports: [TypeOrmModule, TimeslotsService],
+  controllers: [TimeslotsController, ReservationsController],
+  providers: [TimeslotsService, ReservationsService],
+  exports: [TypeOrmModule, TimeslotsService, ReservationsService],
 })
 export class ReservationsModule {}
