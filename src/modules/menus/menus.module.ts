6846970887af2888439e1936b67d1ad3c1436bbc
nahuelmendez6 <mendezmaturano6@gmail.com>
2026-04-20 20:13:00 +0200

    feat: update the MenusModule by adding User to the TypeOrmModule.forFeature list and importing the StockModule. I   will also check if StockModule correctly exports MenuItemService to ensure it can be used within the MenusModule.

--- src/modules/menus/menus.module.ts
+++ src/modules/menus/menus.module.ts
@@ -1,20 +1,23 @@
-import { Module } from '@nestjs/common';
+import { Module, forwardRef } from '@nestjs/common';
 import { TypeOrmModule } from '@nestjs/typeorm';
 import { MenusService } from './menus.service';
-// import { MenusController } from './menus.controller';
 import { Menu } from './entities/menu.entity';
 import { MenuDay } from './entities/menu-day.entity';
 import { MenuOption } from './entities/menu-option.entity';
 import { ShiftModule } from '../shift/shift.module';
 import { Shift } from '../shift/entities/shift.entity';
 import { MenusController } from './menus.controller';
+import { User } from '../users/entities/user.entity';
+import { StockModule } from '../stock/stock.module';
 
 @Module({
   imports: [
-    TypeOrmModule.forFeature([Menu, MenuDay, MenuOption, Shift]),
+    TypeOrmModule.forFeature([Menu, MenuDay, MenuOption, Shift, User]),
     ShiftModule,
+    forwardRef(() => StockModule),
   ],
   controllers: [MenusController],
   providers: [MenusService],
+  exports: [MenusService],
 })
 export class MenusModule {}
